6bfcf624a9a3dd304d3c2a5c176b67668bb80bd3
Axelhri <axelhouairi@gmail.com>
2025-07-03 09:48:34 +0200

    feat(ProductRepository): added product repository

---
+++ src/main/java/e_commerce/e/commerce/product/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package e_commerce.e.commerce.product.repository;
+
+import e_commerce.e.commerce.product.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
