68a345b3ab47e450b44ab843faba2e2238f17462
Axelhri <axelhouairi@gmail.com>
2025-07-03 09:46:34 +0200

    feat(ProductMapper): added product mapper

---
+++ src/main/java/e_commerce/e/commerce/product/mapper/ProductMapper.java
@@ -0,0 +1,22 @@
+package e_commerce.e.commerce.product.mapper;
+
+import e_commerce.e.commerce.product.dto.ProductDTO;
+import e_commerce.e.commerce.product.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public ProductDTO toDto(Product product) {
+        ProductDTO dto = new ProductDTO();
+        dto.setId(product.getId());
+        dto.setName(product.getName());
+        dto.setPrice(product.getPrice());
+        dto.setPrice(product.getRating());
+        return dto;
+    }
+
+    public Product toEntity(ProductDTO dto) {
+        return new Product(dto.getId(), dto.getName(), dto.getPrice(), dto.getRating());
+    }
+}
