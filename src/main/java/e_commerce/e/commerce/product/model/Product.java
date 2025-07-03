37c1242729f8d84c9a120d349e45a7bc91e5c600
Axelhri <axelhouairi@gmail.com>
2025-07-03 09:38:12 +0200

    feat(product): added product entity

---
+++ src/main/java/e_commerce/e/commerce/product/model/Product.java
@@ -0,0 +1,68 @@
+package e_commerce.e.commerce.product.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private Integer price;
+    private Integer rating;
+
+    public Product(UUID id, String name, Integer price, Integer rating) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", rating=" + rating +
+                '}';
+    }
+}
