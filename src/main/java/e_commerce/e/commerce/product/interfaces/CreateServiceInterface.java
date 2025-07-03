246ec90fe01830f59a03ccbe154a9b1faa0fce7a
Axelhri <axelhouairi@gmail.com>
2025-07-03 09:55:27 +0200

    feat(CreateServiceInterface): create service interface added

---
+++ src/main/java/e_commerce/e/commerce/product/interfaces/CreateServiceInterface.java
@@ -0,0 +1,7 @@
+package e_commerce.e.commerce.product.interfaces;
+
+import e_commerce.e.commerce.product.dto.ProductDTO;
+
+public interface CreateServiceInterface {
+    void createProduct(ProductDTO productDTO);
+}
