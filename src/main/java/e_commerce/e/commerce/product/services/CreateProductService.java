fe7fc0b77a2a9c64f10a723ee42ed9930d5f61ad
Axelhri <axelhouairi@gmail.com>
2025-07-03 10:19:18 +0200

    feat(CreateProductService): create product service

---
+++ src/main/java/e_commerce/e/commerce/product/services/CreateProductService.java
@@ -0,0 +1,26 @@
+package e_commerce.e.commerce.product.services;
+
+import e_commerce.e.commerce.product.dto.ProductDTO;
+import e_commerce.e.commerce.product.interfaces.CreateServiceInterface;
+import e_commerce.e.commerce.product.mapper.ProductMapper;
+import e_commerce.e.commerce.product.model.Product;
+import e_commerce.e.commerce.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateProductService implements CreateServiceInterface {
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @Autowired
+    public CreateProductService(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    @Override
+    public void createProduct(ProductDTO productDTO) {
+        productRepository.save(productMapper.toEntity(productDTO));
+    }
+}
